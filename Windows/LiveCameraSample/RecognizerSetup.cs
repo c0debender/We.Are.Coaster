a2e9f5cbec505986494959fa2b7cdc46ef0de494
Rob Mah <rob.mah@tsunamisolutions.com>
2018-11-08 13:17:26 -0800

    first commit

---
+++ Windows/LiveCameraSample/RecognizerSetup.cs
@@ -0,0 +1,20 @@
+﻿namespace LiveCameraSample.Properties
+{
+    internal sealed partial class RecognizerSetup
+    {
+        public RecognizerSetup()
+        {
+
+        }
+
+        private void SettingChangingEventHandler(object sender, System.Configuration.SettingChangingEventArgs e)
+        {
+
+        }
+
+        private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
